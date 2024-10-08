67829d1253676c110e994135f52f431ade81e324
Emily Boudreaux <emily@boudreauxmail.com>
2024-10-08 13:41:11 +0000

    feat(runTests.sh): updated run test script to work for any fork of the libmesac repository

--- docker-files/mesa/runTests.sh
+++ docker-files/mesa/runTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source $MESASDK_ROOT/bin/mesasdk_init.sh
-echo "Cloning libmesac from branch $BRANCH"
-git clone --branch $BRANCH https://github.com/4D-STAR/libmesac.git
+echo "Cloning libmesac from branch $REPOSITORY/$BRANCH"
+git clone --branch $BRANCH $REPOSITORY
 cd libmesac
 ./mk
 cp build/install/bin/test_results.xml ~/results
